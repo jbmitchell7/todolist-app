cbd4e792602ca62a9c82d6ac0444c8aa69c513fb
jbmitchell7 <jbmitchell747@gmail.com>
2021-08-19 20:06:05 -0500

    started jquery section 1 and 2

--- js/script.js
+++ js/script.js
@@ -4,20 +4,18 @@ function newItem(){
     //javascript
     //1. Adding a new item to the list of items: 
        let li = $('<li></li>')
-       let inputValue = $('#input').inputValue;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
+       let inputValue = $('#input').val();
+       li.appendChild(inputValue);
     
        if (inputValue === '') {
          alert("You must write something!");
        } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
+         $('#list').append(li);
        }
     
      //2. Crossing out an item from the list of items:
        function crossOut() {
-             li.classList.toggle("strike");
+             li.toggleClass("strike");
          }
     
          li.addEventListener("dblclick",crossOut);
@@ -49,23 +47,7 @@ function newItem(){
     
     
       /*
-    // jQuery Code
-    //1. Adding a new item to the list:
-    
-      let li = $('<li></li>');
-      let inputValue = $('#input').val();
-      li.append(inputValue);
-    
-      if (inputValue === '') {
-        alert("You must write something!");
-      } else {
-        $('#list').append(li);
-      }
-    //2. Crossing an item out:
-      function crossOut() {
-            li.toggleClass("strike");
-        }
-    
+
         li.on("dblclick", function crossOut() {
             li.toggleClass("strike");
         });
